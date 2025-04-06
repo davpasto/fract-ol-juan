5fad200ca0ab8a6dd1bfdcd9bfc600d9c83c21b8
davpasto <d4vid@DESKTOP-EKSVFFA.localdomain>
2025-04-06 10:15:19 +0200

    aaaa

--- src/draws.c
+++ src/draws.c
@@ -76,12 +76,12 @@ void	calculate_julia(t_fractal *fractal)
 			+ fractal->cx;
 		fractal->zy = 2 * fractal->zy * tmp + fractal->cy;
 		if (fractal->zx * fractal->zx + fractal->zy
-			* fractal->zy >= __DBL_MAX__)
+			* fractal->zy >= 4.0)
 			break ;
 	}
 	if (i == fractal->max_iterations)
 		put_color_to_pixel(fractal, fractal->x, fractal->y, 0x000000);
 	else
 		put_color_to_pixel(fractal, fractal->x, fractal->y, (fractal->color * (i
-						%255)));
+						% 255)));
 }
